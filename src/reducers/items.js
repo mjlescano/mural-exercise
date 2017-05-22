c8e1bcddfd6d8c808f97ecefc9cb9b27402ebded
Matías Lescano <mjlescano@protonmail.com>
2017-05-22 01:35:45 -0300

    update reducers notation

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -3,23 +3,20 @@ import shortid from 'shortid'
 const initialState = []
 
 const reducers = {
-  ADD_ITEM: (state, { payload }) => {
-    const nextState = state.map((item) => {
+  ADD_ITEM: (state, { payload }) => ([
+    ...state.map((item) => {
       if (!item.editing && !item.selected) return item
       return { ...item, editing: false, selected: false }
-    })
-
-    nextState.push({
+    }),
+    {
       kind: 'sticky',
       text: '',
       selected: true,
       editing: true,
       ...payload,
       id: shortid.generate()
-    })
-
-    return nextState
-  },
+    }
+  ]),
 
   REMOVE_ITEM: (state, { payload: { id } }) => (
     state.filter((item) => id !== item.id)
@@ -36,11 +33,20 @@ const reducers = {
     return item
   }),
 
-  SELECT_ITEM: (state, { payload: { id } }) => state.map((item) => {
-    if (item.id === id) return { ...item, selected: true }
-    if (!item.editing && !item.selected) return item
-    return { ...item, selected: false, editing: false }
-  }),
+  SELECT_ITEM: (state, { payload: { id } }) => (
+    state.reduceRight((items, item) => {
+      if (item.id === id) {
+        // Bring selected item to front
+        items.push({ ...item, selected: true })
+      } else if (item.editing || item.selected) {
+        items.unshift({ ...item, selected: false, editing: false })
+      } else {
+        items.unshift(item)
+      }
+
+      return items
+    }, [])
+  ),
 
   UNSELECT_ITEM: (state, { payload: { id } }) => state.map((item) => {
     if (item.id === id) return { ...item, selected: false, editing: false }
@@ -57,25 +63,25 @@ const reducers = {
     return item
   }),
 
-  DUPLICATE_ITEM: (state, { payload: { id } }) => {
-    let original
+  DUPLICATE_ITEM: (state, { payload: { id } }) => (
+    state.reduceRight((items, item) => {
+      if (item.id === id) {
+        items.unshift({ ...item, selected: false, editing: false })
 
-    const items = state.map((item) => {
-      if (item.id !== id) return item
-      original = item
-      return { ...original, selected: false, editing: false }
-    })
+        items.push({
+          ...item,
+          id: shortid.generate(),
+          position: item.position.map((pos) => pos - 30),
+          selected: true,
+          editing: false
+        })
+      } else {
+        items.unshift(item)
+      }
 
-    items.push({
-      ...original,
-      id: shortid.generate(),
-      position: original.position.map((pos) => pos - 30),
-      selected: true,
-      editing: false
-    })
-
-    return items
-  }
+      return items
+    }, [])
+  )
 }
 
 export default (state = initialState, action) => {
